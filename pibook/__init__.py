6c03af3dc20514660367183eb0ba401ad7025a0e
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-02-27 06:51:52 -0500

    Updates init.

--- pibook/__init__.py
+++ pibook/__init__.py
@@ -19,3 +19,16 @@ pibook
 
 Web app for budgeting and financial planning.
 """
+
+from __future__ import absolute_import
+
+from . import models
+
+
+__title__ = 'piboook'
+__version__ = '0.1'
+__author__ = 'Harold Bradley III'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016-2017 Harold Bradley III'
+
+# Soli Deo gloria. <><
